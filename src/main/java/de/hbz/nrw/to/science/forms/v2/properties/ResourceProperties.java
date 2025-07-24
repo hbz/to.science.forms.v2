35f25b38dcabd161c50079ab65848075ac244120
Alessio Pellerito <alessio.pellerito@hbz-nrw.de>
2025-07-24 13:25:47 +0200

    merge issues fixed

--- src/main/java/de/hbz/nrw/to/science/forms/v2/properties/ResourceProperties.java
+++ src/main/java/de/hbz/nrw/to/science/forms/v2/properties/ResourceProperties.java
@@ -1,13 +1,7 @@
 package de.hbz.nrw.to.science.forms.v2.properties;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
-
 import lombok.Data;
 
 /**
@@ -17,12 +11,10 @@ import lombok.Data;
  */
 @Data
 @Configuration
-//@PropertySource(value = "file:/etc/forms/application.properties", ignoreResourceNotFound = true) // for prod
-@PropertySource(value = "file:/etc/forms-dev/application.properties", ignoreResourceNotFound = true) // for dev
 @ConfigurationProperties(prefix = "resource")
 public class ResourceProperties {
   	
-  private String frlUrl;
+	private String frlUrl;
 	private String frlUrlHelptext;
 	private String frlApiUser;
 	private String frlApiPassword;
